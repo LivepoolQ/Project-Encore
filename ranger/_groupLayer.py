b6211428edbcc954c1cec47e417d535d8a3491f4
Ziqian Zou <zouziqianlive@gmail.com>
2025-12-04 16:57:46 +0800

    add socialcircle branch, which is equal to conception module

--- ranger/_groupLayer.py
+++ ranger/_groupLayer.py
@@ -2,7 +2,7 @@
 @Author: Ziqian Zou
 @Date: 2025-11-25 10:28:07
 @LastEditors: Ziqian Zou
-@LastEditTime: 2025-11-25 18:43:37
+@LastEditTime: 2025-12-04 11:14:27
 @Description: file content
 @Github: https://github.com/LivepoolQ
 @Copyright 2025 Ziqian Zou, All Rights Reserved.
@@ -19,10 +19,14 @@ MU = 0.00001
 class GroupLayer(torch.nn.Module):
 
     def __init__(self, 
+                 max_partitions: int,
                  view_angle: float = np.pi,
+                 use_socialcircle: int = 1,
                  *args, **kwargs):
         super().__init__()
         self.view_angle = view_angle
+        self.use_socialcircle = use_socialcircle
+        self.partitions = max_partitions
 
     def forward(self, trajs: torch.Tensor, nei_trajs: torch.Tensor):
         # `nei_trajs` are relative values to target agents' last obs step
@@ -79,37 +83,76 @@ class GroupLayer(torch.nn.Module):
         # calculate neighbor's velocity
         velocity = torch.norm(nei_vector, dim=-1)
 
-        # for neighbors in view angle, the conception layer would consider all three factors
-        # for neighbors in the back, the conception layer would only consider distance factor
-        # calculate conception value in right view
-        dis_right = (torch.sum(dis * nei_right,
-                        dim=[-1, -2])) / (torch.sum(nei_right, dim=-1) + MU)
-        dir_right = (torch.sum(delta_dir * nei_right,
-                        dim=[-1, -2])) / (torch.sum(nei_right, dim=-1) + MU)
-        vel_right = (torch.sum(velocity * nei_right,
-                        dim=[-1, -2])) / (torch.sum(nei_right, dim=-1) + MU)
-        con_right = torch.concat(
-            [dis_right[:, None, None], dir_right[:, None, None], vel_right[:, None, None]], dim=-1)
+        if self.use_socialcircle:
+            # Angles (the independent variable \theta)
+            angle_indices = nei_dir / (2*np.pi/self.partitions)
+            angle_indices = angle_indices.to(torch.int32)
 
-        # calculate conception value in left view
-        dis_left = (torch.sum(dis * nei_left,
-                    dim=[-1, -2])) / (torch.sum(nei_left, dim=-1) + MU)
-        dir_left = (torch.sum(delta_dir * nei_left,
-                    dim=[-1, -2])) / (torch.sum(nei_left, dim=-1) + MU)
-        vel_left = (torch.sum(velocity * nei_left,
-                    dim=[-1, -2])) / (torch.sum(nei_left, dim=-1) + MU)
-        con_left = torch.concat(
-            [dis_left[:, None, None], dir_left[:, None, None], vel_left[:, None, None]], dim=-1)
+            angle_indices = angle_indices * nei_mask + -1 * (1 - nei_mask)
 
-        # calculate conception in the back
-        dis_back = (torch.sum(dis * nei_back,
-                    dim=[-1, -2])) / (torch.sum(nei_back, dim=-1) + MU)
-        con_back = torch.concat([dis_back[:, None, None]], dim=-1) 
+            # Compute the SocialCircle
+            social_circle = []
+            for ang in range(self.partitions):
+                _mask = (angle_indices == ang).to(torch.float32)
+                _mask_count = torch.sum(_mask, dim=-1)
 
-        # add right and left
-        con = torch.concat([con_right, con_left, con_back], dim=-1)
+                n = _mask_count + 0.0001
+                social_circle.append([])
 
-        return con
+                _velocity = torch.sum(velocity * _mask, dim=-1) / n
+                social_circle[-1].append(_velocity)
+
+                _distance = torch.sum(dis * _mask, dim=-1) / n
+                social_circle[-1].append(_distance)
+
+                _direction = torch.sum(nei_dir * _mask, dim=-1) / n
+                social_circle[-1].append(_direction)
+
+            # Shape of the final SocialCircle: (batch, p, 3)
+            social_circle = [torch.stack(i) for i in social_circle]
+            social_circle = torch.stack(social_circle)
+            social_circle = torch.permute(social_circle, [2, 0, 1])
+
+            if (((m := self.partitions) is not None) and
+                    (m > (n := self.partitions))):
+                paddings = torch.tensor([0, 0, 0, m - n, 0, 0])
+                social_circle = torch.nn.functional.pad(social_circle, paddings)
+
+            return social_circle
+
+        else:
+
+            # for neighbors in view angle, the conception layer would consider all three factors
+            # for neighbors in the back, the conception layer would only consider distance factor
+            # calculate conception value in right view
+            dis_right = (torch.sum(dis * nei_right,
+                            dim=[-1, -2])) / (torch.sum(nei_right, dim=-1) + MU)
+            dir_right = (torch.sum(delta_dir * nei_right,
+                            dim=[-1, -2])) / (torch.sum(nei_right, dim=-1) + MU)
+            vel_right = (torch.sum(velocity * nei_right,
+                            dim=[-1, -2])) / (torch.sum(nei_right, dim=-1) + MU)
+            con_right = torch.concat(
+                [dis_right[:, None, None], dir_right[:, None, None], vel_right[:, None, None]], dim=-1)
+
+            # calculate conception value in left view
+            dis_left = (torch.sum(dis * nei_left,
+                        dim=[-1, -2])) / (torch.sum(nei_left, dim=-1) + MU)
+            dir_left = (torch.sum(delta_dir * nei_left,
+                        dim=[-1, -2])) / (torch.sum(nei_left, dim=-1) + MU)
+            vel_left = (torch.sum(velocity * nei_left,
+                        dim=[-1, -2])) / (torch.sum(nei_left, dim=-1) + MU)
+            con_left = torch.concat(
+                [dis_left[:, None, None], dir_left[:, None, None], vel_left[:, None, None]], dim=-1)
+
+            # calculate conception in the back
+            dis_back = (torch.sum(dis * nei_back,
+                        dim=[-1, -2])) / (torch.sum(nei_back, dim=-1) + MU)
+            con_back = torch.concat([dis_back[:, None, None]], dim=-1) 
+
+            # add right and left
+            con = torch.concat([con_right, con_left, con_back], dim=-1)
+
+            return con
 
 
 class LongTermKernel(torch.nn.Module):
