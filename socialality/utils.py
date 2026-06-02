de13b59b8a01968cab5614808cbec7999fd4696d
Ziqian Zou <zouziqianlive@gmail.com>
2026-06-02 09:33:38 +0800

    modify socialality visualization utils

--- socialality/utils.py
+++ socialality/utils.py
@@ -2,7 +2,7 @@
 @Author: Conghao Wong
 @Date: 2025-12-09 15:50:31
 @LastEditors: Ziqian Zou
-@LastEditTime: 2026-04-24 11:27:20
+@LastEditTime: 2026-06-02 09:29:24
 @Github: https://cocoon2wong.github.io
 @Copyright 2025 Conghao Wong, All Rights Reserved.
 """
@@ -88,13 +88,13 @@ class Gate(torch.nn.Module):
         return torch.maximum(x, torch.ones_like(x) * self.gate)
     
     
-def vis_socialality(anchors:torch.Tensor, IDs=None):
+def vis_socialality_o(anchors:torch.Tensor, IDs=None):
 
     plt.close('Socialality Anchors')
     plt.figure('Socialality Anchors', figsize=(4, 4))
 
-    import seaborn as sns
-    sns.kdeplot(x=anchors.numpy().T[0], y=anchors.numpy().T[1], fill=True, alpha=0.3)
+    # import seaborn as sns
+    # sns.kdeplot(x=anchors.numpy().T[0], y=anchors.numpy().T[1], fill=True, alpha=0.3)
 
     scatter = plt.scatter(
         anchors[:, 0],
@@ -123,3 +123,66 @@ def vis_socialality(anchors:torch.Tensor, IDs=None):
 
     return anchors
 
+def vis_socialality(anchors: torch.Tensor, IDs=None):
+    import numpy as np
+    import matplotlib.pyplot as plt
+    import matplotlib.colors as mcolors
+
+    plt.close('Socialality Anchors')
+    plt.figure('Socialality Anchors', figsize=(4, 4))
+
+    anchors_np = anchors.detach().cpu().numpy()
+    x = anchors_np[:, 0]
+    y = anchors_np[:, 1]
+
+    # Normalization
+    x_norm = (x - x.min()) / (x.max() - x.min() + 1e-8)
+    y_norm = (y - y.min()) / (y.max() - y.min() + 1e-8)
+
+    # x: purple -> pink -> orange -> yellow
+    cmap = mcolors.LinearSegmentedColormap.from_list(
+        'purple_to_yellow',
+        [
+            '#5B4EA3',
+            '#A35AA3',
+            '#E56B8A',
+            '#FF8C42',
+            '#FFC300'
+        ]
+    )
+    base_colors = cmap(x_norm)[:, :3]
+
+    # y: controls the degree of white color, small y indicates big degree
+    white = np.ones_like(base_colors)
+    mix = 0.25 + 0.75 * y_norm[:, None]   
+    final_colors = white * (1 - mix) + base_colors * mix
+
+    scatter = plt.scatter(
+        x,
+        y,
+        c=final_colors,
+        s=30,
+        alpha=0.9
+    )
+
+    if IDs:
+        import mplcursors
+
+        cursor = mplcursors.cursor(scatter, hover=True)
+
+        @cursor.connect("add")
+        def on_add(sel):
+            label = IDs[sel.index]
+            sel.annotation.set_text(label)
+            sel.annotation.get_bbox_patch().set(fc="gray", alpha=0.5)
+
+    plt.xlabel(r'$\tau^{a}$')
+    plt.ylabel(r'$\tau^{b}$')
+    plt.title('Socialality Anchors')
+    # plt.axis('equal')
+    plt.tight_layout()
+    plt.xlim((-0.1, 0.1))
+    plt.ylim((0.61, 0.71))
+    plt.show()
+
+    return anchors
